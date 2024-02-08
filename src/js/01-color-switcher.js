73cbe3077a41d8d8c830ef395c6715e14b4c75e0
MarekLempart <kama.zakupy@gmail.com>
2024-02-08 14:56:51 +0100

    Praca nad zadaniem domowym 09- część 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBut = document.querySelector('[data-start]');
+const stopBut = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId = null;
+
+startBut.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBut.disabled = true;
+  stopBut.disabled = false;
+});
+
+stopBut.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startBut.disabled = false;
+  stopBut.disabled = true;
+});
